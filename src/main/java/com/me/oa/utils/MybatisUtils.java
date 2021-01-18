ef98a60311c7f521eb01ffb4e4daa829989c3e8e
liuchenyang0515 <897418643@qq.com>
2021-01-18 15:56:02 +0800

    3-3 开发MyBatisUtils-2

--- src/main/java/com/me/oa/utils/MybatisUtils.java
+++ src/main/java/com/me/oa/utils/MybatisUtils.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -12,6 +13,7 @@ import java.util.function.Function;
 public class MybatisUtils {
     // 利用static(静态)属于类不属于对象，且全局唯一
     private static SqlSessionFactory sqlSessionFactory = null;
+
     // 利用静态块在初始化类时实例化sqlSessionFactory
     static {
         Reader reader = null;
@@ -25,7 +27,42 @@ public class MybatisUtils {
         }
     }
 
+
+    /**
+     * 执行SELECT查询SQL
+     *
+     * @param func 要执行查询语句的代码块
+     * @return 查询结果
+     */
     public static Object executeQuery(Function<SqlSession, Object> func) {
-        return null;
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            Object obj = func.apply(sqlSession);
+            return obj;
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    /**
+     * 执行INSERT/UPDATE/DELETE写操作SQL
+     *
+     * @param func 要执行的写操作代码块
+     * @return 写操作后返回的结果
+     */
+    @Test
+    public static Object executeUpdate(Function<SqlSession, Object> func) {
+        // 写操作和查询操作区别就是对事物的控制。这里在上一步查询基础上增加了事务的控制
+        SqlSession sqlSession = sqlSessionFactory.openSession(false); // 不自动提交
+        try {
+            Object obj = func.apply(sqlSession);
+            sqlSession.commit();
+            return obj;
+        } catch (RuntimeException e) {
+            sqlSession.rollback();
+            throw e;
+        } finally {
+            sqlSession.close();
+        }
     }
 }
