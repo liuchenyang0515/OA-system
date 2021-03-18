08b21a262cd91e245e4c02703a1793ab0de5b049
liuchenyang0515 <897418643@qq.com>
2021-03-18 19:30:45 +0800

    5-1 基于LayUI开发登录页--下载存放layUI所需资源到resources目录；添加Druid连接池的一点注释

--- src/main/java/com/me/oa/datasource/DruidDataSourceFactory.java
+++ src/main/java/com/me/oa/datasource/DruidDataSourceFactory.java
@@ -7,7 +7,9 @@ import javax.sql.DataSource;
 import java.sql.SQLException;
 
 public class DruidDataSourceFactory extends UnpooledDataSourceFactory {
-    public DruidDataSourceFactory() {
+    public DruidDataSourceFactory() { // UnpooledDataSourceFactory内部默认赋值了一个不需要连接池就能访问的数据源，子类重写实例化DataSource对象
+        // 1.创建空的数据源对象 2.执行setProperties读取数据原配置进行属性设置 3.（可选）重写getDataSource，进行datasource初始化
+        // 执行以上3步骤后，mybatis在执行时就会利用指定的datasourcefactory来获取数据源了
         this.dataSource = new DruidDataSource();
     }
 
--- src/test/java/com/me/oa/utils/MybatisUtilsTestor.java
+++ src/test/java/com/me/oa/utils/MybatisUtilsTestor.java
@@ -7,12 +7,12 @@ public class MybatisUtilsTestor {
     @Test
     public void testcase1() {
         String result = (String) MybatisUtils.executeQuery(sqlSession -> {
-            String out = (String) sqlSession.selectOne("test.sample");
+            String out = (String) sqlSession.selectOne("test.sample"); // 命名空间.sql的id
             return out;
         });
         System.out.println(result);
     }
-
+    // 这两种写法一样
     @Test
     public void testcase2() {
         String result = (String) MybatisUtils.executeQuery(sqlSession -> sqlSession.selectOne("test.sample"));
