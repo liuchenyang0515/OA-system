ceea563fdd04fd82f948ae0301027ee988cc7a43
liuchenyang0515 <897418643@qq.com>
2021-03-23 17:01:59 +0800

    8-1 实现待审批请假单列表1--实现四表关联查询，给定查询条件(complete、process、ready)和经办人id(operator_id)，查询经办人该审批状态的假单，并查出提交假单的员工姓名和部门

--- src/main/java/com/me/oa/dao/LeaveFormDao.java
+++ src/main/java/com/me/oa/dao/LeaveFormDao.java
@@ -1,7 +1,12 @@
 package com.me.oa.dao;
 
 import com.me.oa.entity.LeaveForm;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 public interface LeaveFormDao {
     public void insert(LeaveForm form);
+    public List<Map> selectByParams(@Param("pf_state") String pfState, @Param("pf_operator_id") Long operatorOd);
 }
--- src/test/java/com/me/oa/dao/LeaveFormDaoTest.java
+++ src/test/java/com/me/oa/dao/LeaveFormDaoTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class LeaveFormDaoTest {
 
@@ -33,6 +35,27 @@ public class LeaveFormDaoTest {
             form.setState("processing"); // 当前状态
             dao.insert(form);
             return null;
+        });
+    }
+
+    /**
+     * 实现四表关联查询，给定查询条件(complete、process、ready)和经办人id(operator_id)，查询经办人该审批状态的假单，并查出提交假单的员工姓名和部门
+     * <p>
+     * [main] 16:49:33.951 DEBUG c.m.o.d.LeaveFormDao.selectByParams - ==>  Preparing: SELECT f.*, e.name, d.department_name FROM adm_leave_form f,
+     * adm_process_flow pf, adm_employee e, adm_department d WHERE f.form_id = pf.form_id AND f.employee_id = e.employee_id
+     * AND e.department_id = d.department_id AND pf.state = ? AND pf.operator_id = ?
+     * [main] 16:49:34.096 DEBUG c.m.o.d.LeaveFormDao.selectByParams - ==> Parameters: process(String), 2(Long)
+     * [main] 16:49:34.133 DEBUG c.m.o.d.LeaveFormDao.selectByParams - <==      Total: 1
+     * [{start_time=2021-03-01 08:00:00.0, reason=测试, create_time=2021-03-23 15:20:29.0, employee_id=4, department_name=研发部, form_id=43,
+     * end_time=2021-03-19 18:00:00.0, name=宋彩妮, form_type=4, state=processing}]
+     */
+    @Test
+    public void testSelectByParams() {
+        MybatisUtils.executeUpdate(sqlSession -> {
+            LeaveFormDao dao = sqlSession.getMapper(LeaveFormDao.class);
+            List<Map> list = dao.selectByParams("process", 2l);
+            System.out.println(list);
+            return list;
         });
     }
 }
